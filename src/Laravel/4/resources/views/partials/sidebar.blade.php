9b6cafa242910872391eae9b942daed02b40281f
ramosj <ramosjhonzylvin@gmail.com>
2018-04-27 16:57:40 +0800

    04272018

--- src/Laravel/4/resources/views/partials/sidebar.blade.php
+++ src/Laravel/4/resources/views/partials/sidebar.blade.php
@@ -1,18 +1,9 @@
-@inject('request', 'Illuminate\Http\Request')
 <!-- Left side column. contains the sidebar -->
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <ul class="sidebar-menu">
 
-             
-
-            <li class="{{ $request->segment(1) == 'home' ? 'active' : '' }}">
-                <a href="{{ url('/') }}">
-                    <i class="fa fa-wrench"></i>
-                    <span class="title">@lang('quickadmin.qa_dashboard')</span>
-                </a>
-            </li>
 
             
             @can('user_management_access')
@@ -72,4 +63,6 @@
         </ul>
     </section>
 </aside>
+
+
 
